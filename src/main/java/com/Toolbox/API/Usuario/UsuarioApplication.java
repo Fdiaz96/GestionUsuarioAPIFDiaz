2439c2eef44eab37ef86be631b49016a4cefb296
Fdiaz96 <93749437+Fdiaz96@users.noreply.github.com>
2021-11-07 13:04:18 -0300

    Deploy 1.0

--- src/main/java/com/Toolbox/API/Usuario/UsuarioApplication.java
+++ src/main/java/com/Toolbox/API/Usuario/UsuarioApplication.java
@@ -20,7 +20,7 @@ public class UsuarioApplication {
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("https://gestion-de-usuarios.000webhostapp.com"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("https://gestion-de-usuarios.000webhostapp.com", "http://localhost:8080/"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers"));

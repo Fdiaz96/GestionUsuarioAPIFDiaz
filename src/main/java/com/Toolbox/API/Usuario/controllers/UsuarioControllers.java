773f814afffa00e31551c17957e1ef00567b1aaf
Fdiaz96 <93749437+Fdiaz96@users.noreply.github.com>
2021-11-07 12:01:02 -0300

    Puesta en produccion

--- src/main/java/com/Toolbox/API/Usuario/controllers/UsuarioControllers.java
+++ src/main/java/com/Toolbox/API/Usuario/controllers/UsuarioControllers.java
@@ -21,19 +21,20 @@ Usuario usuario= usuarioDao.getUsuarioPorCI(cedula);
 
 return usuario;
     }
+    @CrossOrigin(origins="https://gestion-de-usuarios.000webhostapp.com/")
 @RequestMapping(value="api/obtenerListaUsuarios")
     public List<Usuario> obtenerListaUsuarios() {
         return usuarioDao.obtenerListaUsuarios();
 
     }
-
+    @CrossOrigin(origins="https://gestion-de-usuarios.000webhostapp.com/")
     @RequestMapping(value = "api/usuarios/{id}", method = RequestMethod.DELETE)
     public void eliminar(@PathVariable int id) {
 
         usuarioDao.eliminar(id);
 
     }
-
+    @CrossOrigin(origins="https://gestion-de-usuarios.000webhostapp.com/")
     @RequestMapping(value = "api/usuarios", method = RequestMethod.POST)
     public void registrarUsuario(@RequestBody Usuario usuario) {
 

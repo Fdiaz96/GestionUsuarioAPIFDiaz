aa0544b022b8118687fc3a3c3979b8edd6181402
Fdiaz96 <93749437+Fdiaz96@users.noreply.github.com>
2021-11-07 12:53:12 -0300

    Deploy 1.1

--- src/main/java/com/Toolbox/API/Usuario/controllers/UsuarioControllers.java
+++ src/main/java/com/Toolbox/API/Usuario/controllers/UsuarioControllers.java
@@ -13,7 +13,6 @@ public class UsuarioControllers {
 @Autowired
 private UsuarioDao usuarioDao;
 
-    @CrossOrigin(origins="https://gestion-de-usuarios.000webhostapp.com/")
     @RequestMapping(value = "api/obtenerUsuario/{cedula}", method = RequestMethod.GET)
     public Usuario obtenerUsuariosPorMail(@PathVariable String cedula) {
 
@@ -22,20 +21,20 @@ Usuario usuario= usuarioDao.getUsuarioPorCI(cedula);
 
 return usuario;
     }
-    @CrossOrigin(origins="https://gestion-de-usuarios.000webhostapp.com/")
+
 @RequestMapping(value="api/obtenerListaUsuarios")
     public List<Usuario> obtenerListaUsuarios() {
         return usuarioDao.obtenerListaUsuarios();
 
     }
-    @CrossOrigin(origins="https://gestion-de-usuarios.000webhostapp.com/")
+
     @RequestMapping(value = "api/usuarios/{id}", method = RequestMethod.DELETE)
     public void eliminar(@PathVariable int id) {
 
         usuarioDao.eliminar(id);
 
     }
-    @CrossOrigin(origins="https://gestion-de-usuarios.000webhostapp.com/")
+
     @RequestMapping(value = "api/usuarios", method = RequestMethod.POST)
     public void registrarUsuario(@RequestBody Usuario usuario) {
 
